6f372e9291f2403429eedcb360615c839f4ad4d2
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-02-22 12:05:22 +0100

    Update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,7 @@ import menuTemplate from '../templates/menu-items.hbs';
 import menuData from '../menu.json';
 
 const markup = menuTemplate(menuData);
+
 const menuRef = document.querySelector('.js-menu');
 
 menuRef.insertAdjacentHTML('beforeend', markup);
